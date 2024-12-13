1bc4f4736411b756c3a1e129c832bb5879dc963e
miguelAngel48 <miguelangelponcesanchez@yahoo.es>
2024-12-13 21:59:17 +0100

    Try second commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,15 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println( isAnagram("marmol", "lomram"));
+
+    }
+
+    public static boolean isAnagram(String a, String b) {
+        String fraseA = "";
+        String fraseB = "";
+        for (int i = 0; i < a.length(); i++) {
+            char c = a.charAt(i);
+        }
+        return fraseA.equals(fraseB);
     }
 }
